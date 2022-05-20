361917a315e50f40b6149639d958578b36f3a44a
MaxxZimm <101809163+MaxxZimm@users.noreply.github.com>
2022-05-20 10:36:20 -0300

    Parte1 plataformas

---
+++ Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+
+    public float xSpeed;
+    private bool right;
+
+    public GameObject prefab1;
+    public GameObject prefab2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        right = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(right == true){
+            transform.position += new Vector3(xSpeed, 0, 0);
+        }
+        else
+        {
+            transform.position += new Vector3(-xSpeed, 0, 0);
+        }
+        if(transform.position.x > 1.85)
+        {
+            right = false;
+            Instantiate(prefab1);
+        }
+        else if (transform.position.x < -1.85)
+        {
+            right = true;
+            Instantiate(prefab2);
+        }
+
+    }
+}
